8c3bcc4bf9a63703cb59aa9a9d83751c9fc85417
Olga771 <135791695+Olga771@users.noreply.github.com>
2024-02-27 19:40:52 -0500

    Added bsic form handling and added typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  let poemElement = document.querySelector("#poem");
+
+  new Typewriter("#poem", {
+    strings: "This will be the poem here",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
