1e8d52cc9a64065a43dbbb532ab44f053d634012
Olga771 <135791695+Olga771@users.noreply.github.com>
2024-02-27 20:45:25 -0500

    Added AI API Integration

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
-function generatePoem(event) {
-  event.preventDefault();
-
-  let poemElement = document.querySelector("#poem");
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "This will be the poem here",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
 }
 
-let poemFormElement = document.querySelector("#poem-generator-form");
-poemFormElement.addEventListener("submit", generatePoem);
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "7f4590fo354t54d904d24b1af975d02a";
+  let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
+  let context =
+    "You are a romantic poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line witha </br>. Make sure to follow user instructions. Do not include a title to the poem. Sign the poem with  'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the begining";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating poem");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiURL).then(displayPoem);
+
+  let poemFormElement = document.querySelector("#poem-generator-form");
+  poemFormElement.addEventListener("submit", generatePoem);
+}
